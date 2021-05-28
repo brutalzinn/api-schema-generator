cf6bbbf87a40c6325d3620af5a3200c28b402888
=Roberto Paes <roberto.paes@al.infnet.edu.br>
2021-05-27 22:41:44 -0300

    route extra added

--- api/services/search.service.js
+++ api/services/search.service.js
@@ -2,15 +2,97 @@ const {openFile,createModel,Insert,Update,Delete} = require('../utils/database.u
 const databaseUtil = require('../utils/database.assist.utils')
 
 const getDatabaseInfo = async (database,params) => {
+    var searchParams = params.query.query.normalize('NFD').replace(/[\u0300-\u036f]/g, "").toLocaleLowerCase().split(' ');
+    console.log(searchParams)
+    let id = params.query.id
+    let databaseConfig = await databaseUtil.openCustomDatabase(database)
+    let relations = databaseConfig['relation']
+    var response = []
+    if(!relations)
+    {
+        return
+    }
+    let json = await openFile(database)
     
-    var searchParams = params.query.query.normalize('NFD').replace(/[\u0300-\u036f]/g, "").toUpperCase().split(' ');
-let databaseConfig = await databaseUtil.openCustomDatabase(database)
-let relations = databaseConfig['relation']
-//console.log('config',databaseConfig,relationalData)
-    console.log('params ', searchParams,relations)
-    searchParams.map((item)=>{
-        console.log(item)
-    })
+    if(id){
+        json = json.find((g)=>g.id === id)
+    }
+    
+    
+    
+    await Promise.all(relations.map(async (relat)=>{
+        if(searchParams.includes(relat.table)){
+            if(json[relat.key]){
+                if(Array.isArray(json[relat.key])){
+                    let relationTable = await openFile(relat.table)
+                    if(!relationTable){
+                        return
+                    }
+                    let tmpRelationArray = []
+                    json[relat.key].map((v)=>{
+                        let relationFinded = relationTable.find((f)=>f.id === v)
+                        tmpRelationArray.push(relationFinded)
+                    })
+                    json = {...json,[relat.key]:tmpRelationArray}
+                    
+                }else{  
+                    let relationTable = await openFile(relat.table)
+                    
+                    if(!relationTable){
+                        return
+                    }
+                    let relationFinded = relationTable.find((f)=>f.id === json[relat.key])
+                    json = {...json,[relat.key]:relationFinded}
+                    
+                }
+                
+            }
+        }
+    }))
+    
+    
+    if(!Array.isArray(json)){
+        return await json
+    }
+    await Promise.all(json.map(async (item,index)=>{
+        await Promise.all(relations.map(async (relat)=>{
+            if(searchParams.includes(relat.table)){
+                if(item[relat.key]){
+                    if(Array.isArray(item[relat.key])){
+                        let relationTable = await openFile(relat.table)
+                        if(!relationTable){
+                            return
+                        }
+                        let tmpRelationArray = []
+                        item[relat.key].map((v)=>{
+                            let relationFinded = relationTable.find((f)=>f.id === v)
+                            tmpRelationArray.push(relationFinded)
+                        })
+                        json[index] = {...json[index],[relat.key]:tmpRelationArray}
+                        
+                    }else{  
+                        let relationTable = await openFile(relat.table)
+                        
+                        if(!relationTable){
+                            return
+                        }
+                        let relationFinded = relationTable.find((f)=>f.id === item[relat.key])
+                        json[index] = {...json[index],[relat.key]:relationFinded}
+                        
+                    }
+                    
+                }
+            }
+        }))
+    }))
+    
+    
+    
+    return await json
+    // console.log('params ', searchParams,relations)
+    //     searchParams.map((item)=>{
+    //         console.log(item)
+    //     })
     // const json = await openFile(database)
     //  return json.find((item)=>item[column] == value)
 }
