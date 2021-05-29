997384a90f2e5cb00121f9a84a3b27fb4c8adcc1
=Roberto Paes <roberto.paes@al.infnet.edu.br>
2021-05-29 19:08:29 -0300

    update language system

--- cli/commands/language.js
+++ cli/commands/language.js
@@ -1,8 +1,14 @@
 const {saveFile,openFile} = require('../../api/utils/database.assist.utils')
+const LangueUtils = require('../utils/language.utils')
 
+const getLanguage = async (args) =>{
+    await LangueUtils.loadLanguage()
+    return LangueUtils.getLanguage(args)
+}
 const language = async (myArgs) =>{
     let databases = await openFile('config')
     let configIndex = databases.findIndex((v)=>v.config)
+    let langResponse = await getLanguage([myArgs[1]])
     if(configIndex == -1){
         databases.push({config:{language:myArgs[1]}})
     }
@@ -12,6 +18,7 @@ const language = async (myArgs) =>{
         }
     })
     saveFile('config',databases)
+    console.log(langResponse['LANGUAGE_CHANGED'])
 }
 
 module.exports = {
